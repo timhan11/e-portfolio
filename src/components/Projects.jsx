96af380380f1d28482f0f78bdd1295c843c20a9c
Tim Hansen <117866463+timhan11@users.noreply.github.com>
2023-05-13 21:39:39 -0400

    pdf

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,8 +10,8 @@ const Projects = () => {
     <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16    bg-blue opacity-90">
       <h1 className="text-4xl opacity-90 font-bold text-center text-white mb-8">Projects</h1>
       <div className="grid sm:grid-cols-2 gap-12">
-       <ProjectItem ghub="https://github.com/timhan11/weatherapp" link="https://timhan11.github.io/weatherapp/" img={weather} tools="HTMl, CSS, JavaScript, React, Vite, Axios" title="Weather App"/>
-       <ProjectItem ghub="https://github.com/timhan11/ChatAway" link="https://chat-away-lac.vercel.app/"  img={chataway} tools="HTMl, Tailwind CSS, JavaScript, React, Firebase, Vite"  title="Weathere App"/>
+       <ProjectItem ghub="https://github.com/timhan11/weatherapp" link="https://timhan11.github.io/weatherapp/" img={weather} tools="HTMl, CSS, JavaScript, React, Vite, Axios" title="Weather"/>
+       <ProjectItem ghub="https://github.com/timhan11/ChatAway" link="https://chat-away-lac.vercel.app/"  img={chataway} tools="HTMl, Tailwind CSS, JavaScript, React, Firebase, Vite"  title="ChatAway"/>
       
        
       </div>
